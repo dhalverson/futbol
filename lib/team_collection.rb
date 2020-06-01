4457d7a050254c1b85e37d90a2ffea8ca4d060db
Cam C <ckc.cameron@gmail.com>
2020-05-31 19:23:17 -0600

    Adds all method to collection classes

--- lib/team_collection.rb
+++ lib/team_collection.rb
@@ -2,16 +2,24 @@ require 'CSV'
 require_relative 'team'
 
 class TeamCollection
+
+  @@all = []
+
+  def from_csv(teams_file_path)
+    teams = CSV.read(teams_file_path, headers: true, header_converters: :symbol)
+    all_teams = teams.map do |row|
+      Team.new(row)
+    end
+    @@all = all_teams
+  end
+
+  def self.all
+    @@all
+  end
+
   attr_reader :teams
 
   def initialize(teams_file_path)
     @teams = from_csv(teams_file_path)
-  end
-
-  def from_csv(teams_file_path)
-    teams = CSV.read(teams_file_path, headers: true, header_converters: :symbol)
-    teams.map do |row|
-      Team.new(row)
-    end
   end
 end
